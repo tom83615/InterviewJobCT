6071fd47acc63e8780933b5a7d167389910a854c
Hsieh,YuHsuan <tom83615@gmail.com>
2022-04-09 14:26:02 +0800

    Spring boot init

---
+++ src/main/java/com/interview/ct/InterviewJobCtApplication.java
@@ -0,0 +1,13 @@
+package com.interview.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewJobCtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewJobCtApplication.class, args);
+	}
+
+}
